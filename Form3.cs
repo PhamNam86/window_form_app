7edfd802c5661ee528fbcc2125af5414f7f5db45
Pham_Nam <97910573+PhamNam86@users.noreply.github.com>
2023-04-02 13:24:53 +0700

    Add files via upload

---
+++ Form3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrystalReports
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void crystalReportViewer2_Load(object sender, EventArgs e)
+        {
+            CrystalReport1 crpt = new CrystalReport1();
+            crystalReportViewer2.ReportSource = null;
+            crystalReportViewer2.ReportSource = crpt;
+        }
+    }
+}
